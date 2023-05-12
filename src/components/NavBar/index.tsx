022343ebefd279723159f1d0eb8b211b65ffa412
jvoliveirag <jv.oliveira@gmail.com>
2023-05-12 01:46:34 -0300

    style: Estilização na fonte da barra de navegação.

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -33,7 +33,7 @@ const NavBar: React.FC<NavBarProps> = ({ linkName, linkPath }) => {
                 </button>
 
                 <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-                    <ul className="flex flex-col font-medium text-xl mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+                    <ul className="flex flex-col font-semibold text-xl mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                         <li>
                             <a href={linkPath[0]} className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-400 md:p-0 md:hover:underline md:hover:underline-offset-8 duration-200 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{linkName[0]}</a>
                         </li>
