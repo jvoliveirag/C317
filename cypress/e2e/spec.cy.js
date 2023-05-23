65bb752aceec45119ca862d19e0c342d0f6d4f68
jvoliveirag <jv.oliveira@gmail.com>
2023-05-23 15:30:49 -0300

    test | refactor: refatorando e adicionando testes.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,15 @@
-describe('Cenário de teste: Testar funcionalidade do botao de inicio', () => {
-  it('Caso de teste: Ir para a etapa seguinte (filtro de indicação).', () => {
-      cy.visit('https://pickme-vert.vercel.app/');
-      cy.get('button')
-      .contains('Surpreenda-me')
-      .click(); // Clica no botão "Surpreenda-me"
-
+describe('Teste dos botões da tela de início', () => {
+  beforeEach(() => {
+    cy.visit('https://pickme-vert.vercel.app/');
+  });
+  
+  it('Testar botão "Começar": deve redirecionar para a etapa seguinte (filtro de indicação).', () => {
+    cy.contains('Começar').click();
+    cy.url().should('include', '/preferences'); // Verifica se a URL redirecionou para a página "preferences"
+  });
+  
+  it('Testar botão "Surpreenda-me": deve redireconar para a página de recomendação.', () => {
+    cy.contains('Surpreenda-me').click();
     cy.url().should('include', '/recommendation'); // Verifica se a URL redirecionou para a página "recommendation"
-  })
-})
+  });
+});
