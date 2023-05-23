330584bce878babe21df8e378cefec041960d344
jvoliveirag <jv.oliveira@gmail.com>
2023-05-23 17:44:01 -0300

    test: adicionando os scripts necessários para a geração de relatórios e refactor dos casos de teste.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,18 @@
+const APP_LINK = 'https://pickme-vert.vercel.app/' //link da aplicacao em prod
+
 describe('Teste dos botões da tela de início', () => {
   beforeEach(() => {
-    cy.visit('https://pickme-vert.vercel.app/');
+    cy.visit(APP_LINK); 
   });
   
   it('Testar botão "Começar": deve redirecionar para a etapa seguinte (filtro de indicação).', () => {
     cy.contains('Começar').click();
-    cy.url().should('include', '/preferences'); // Verifica se a URL redirecionou para a página "preferences"
+    cy.url().should('include', '/preferences'); // Verifica se a URL redirecionou para a pagina "preferences"
   });
   
   it('Testar botão "Surpreenda-me": deve redireconar para a página de recomendação.', () => {
     cy.contains('Surpreenda-me').click();
-    cy.url().should('include', '/recommendation'); // Verifica se a URL redirecionou para a página "recommendation"
+    cy.url().should('include', '/recommendation'); // Verifica se a URL redirecionou para a pagina "recommendation"
   });
 });
+
