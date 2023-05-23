4a086a09510b4469acd203feea3272b184853775
jvoliveirag <jv.oliveira@gmail.com>
2023-05-23 15:20:19 -0300

    test: ajustando script de testes.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,10 @@
 describe('Cenário de teste: Testar funcionalidade do botao de inicio', () => {
   it('Caso de teste: Ir para a etapa seguinte (filtro de indicação).', () => {
-      cy.visit('http://localhost:8080/');
-      cy.get('button').click();
-      //expect(arguments).to.be.arguments;
-      //expect(URL).to.be.equal('localhost:8080/Vamos-la');
+      cy.visit('https://pickme-vert.vercel.app/');
+      cy.get('button')
+      .contains('Surpreenda-me')
+      .click(); // Clica no botão "Surpreenda-me"
+
+    cy.url().should('include', '/recommendation'); // Verifica se a URL redirecionou para a página "recommendation"
   })
 })
