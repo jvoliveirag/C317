ccb2e0507a4e4193928e630b2759aeaf4e6f3b1b
jvoliveirag <jv.oliveira@gmail.com>
2023-05-23 21:49:47 -0300

    test: adicionando testes da página de recomedação e links para ambiente de dev e prod.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,13 @@
-const APP_LINK = 'https://pickme-vert.vercel.app/' //link da aplicacao em prod
+import { FaRedo, FaHome, FaBookmark, FaArrowLeft } from "react-icons/fa";
+import React from "react";
+
+const APP_LINK = 'https://pickme-vert.vercel.app/'  //link da aplicacao em prod
+const DEV_LINK = 'http://localhost:3000/'           //link ambiente dev
+const TEST_LINK = DEV_LINK //colocar o link do ambiente que deseja testar
 
 describe('Teste dos botões da tela de início', () => {
   beforeEach(() => {
-    cy.visit(APP_LINK); 
+    cy.visit(TEST_LINK); 
   });
   
   it('Testar botão "Começar": deve redirecionar para a etapa seguinte (filtro de indicação).', () => {
@@ -16,3 +21,36 @@ describe('Teste dos botões da tela de início', () => {
   });
 });
 
+describe('Teste dos botões da página de recomendação', () => {
+  beforeEach(() => {
+    cy.visit(`${TEST_LINK}recommendation`); // Substitua pela URL do seu website
+  });
+
+  it("Refazer: gera uma nova indicação (recarrega a página).", () => {
+    cy.get("[id='redo-button']") // Seletor para o elemento 
+    .click();
+
+    cy.url().should("include", "/recommendation");
+  });
+
+  it("Home: redireciona para a página inicial.", () => {
+    cy.get("[id='home-button']") // Seletor para o elemento 
+    .click();
+
+    cy.url().should("include", "/");
+  });
+
+  it("Salvar: caso o usuário não esteja logado redireciona para a página de login, caso esteja para a página de salvos.", () => {
+    cy.get("[id='save-button']") // Seletor para o elemento 
+    .click();
+
+    cy.url().should("include", "/login"); //implementar logica de caso esteja logado
+  });
+
+  it("Voltar: redireciona para a página anterior.", () => {
+    cy.get("[id='previous-button']") // Seletor para o elemento 
+    .click();
+
+    cy.url().should("include", "/filters");
+  });
+});
